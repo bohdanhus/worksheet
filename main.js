6b06f2ccf3a743b91a3ce718bf2a638975a98f25
Bogdan <bromanovich99@gmail.com>
2021-04-13 15:10:22 +0300

    Second com: adding basic logic

--- main.js
+++ main.js
@@ -1,52 +1,48 @@
-const fs = require('fs');
-const path = require('path');
-const csv = require('csv-parser');
-const results = [];
+const fs = require("fs");
+//парсим локальный файл и сохраняем результат в переменной = data;
+const data = fs
+  .readFileSync("acme_worksheet.csv")
+  .toString()
+  .split("\n")
+  .map((e) => e.trim())
+  .map((e) => e.split(",").map((e) => e.trim()));
 
-fs.createReadStream('acme_worksheet.csv')
-  .pipe(csv())
-  .on('data', (data) => results.push(data))
-  .on('end', () => {
-  // console.log(results);
-   someFunction(results);  
-  });
-
-const  someFunction = (data) => {
-const CSV = data;
-// Полноценная таблица
-const table = CSV.trim().split(/[\n\r]+/g).map((row) => row.trim().split(/,+/g))
-// Отделяем типы столбцов
-const columns = table.shift()
+// отделяем типы стобцов
+const columns = data.shift();
 // Определяем столбцы
-const columnName = columns.findIndex((v) => /Name/i.test(v))
-const columnDate = columns.findIndex((v) => /Date/i.test(v))
-const columnWork = columns.findIndex((v) => /Work/i.test(v))
-
+const columnName = columns.findIndex((v) => /Name/i.test(v));
+const columnDate = columns.findIndex((v) => /Date/i.test(v));
+const columnWork = columns.findIndex((v) => /Work/i.test(v));
 // Сортируем дату
-const A = table.reduce((a, v) => {
-  const date = (new Date(v[columnDate])).valueOf()
-  a.includes(date) || a.push(date)
-  return a
-}, []).sort()
+const A = data
+  .reduce((a, v) => {
+    const date = new Date(v[columnDate]).valueOf();
+    a.includes(date) || a.push(date);
+    return a;
+  }, [])
+  .sort();
 
-const employees = table.reduce((a, v) => {
-  const name = v[columnName].trim().replace(/\s+/g, ' ')
-  const date = (new Date(v[columnDate])).valueOf()
-  const work = v[columnWork].trim()
-  let nw = [...a.entries()].find(([re]) => re.test(name))
+const employees = data.reduce((a, v) => {
+  const name = v[columnName].trim().replace(/\s+/g, " ");
+  const date = new Date(v[columnDate]).valueOf();
+  const work = v[columnWork].trim();
+  let nw = [...a.entries()].find(([re]) => re.test(name));
+  // Если сотрудник не работал(а) в один из дней
   if (!nw) {
-    // Здесь можно чем-то заполнить если не работали
-    nw = [null, { name, work: (new Array(A.length)).fill('0') }]
-    a.set(new RegExp(name, 'i'), nw[1])
+    nw = [null, { name, work: new Array(A.length).fill("0") }];
+    a.set(new RegExp(name, "i"), nw[1]);
   }
-  nw[1].work[A.indexOf(date)] = work
-  return a
-}, new Map())
+  nw[1].work[A.indexOf(date)] = work;
+  return a;
+}, new Map());
 
-// Далее делаем что угодно
-const result = [...employees.entries()].map(([_, { name, work }]) => [name, ...work])
-// Наши заголовки с датой
-result.unshift(['Employee', ...A.map((d) => (new Date(d).toDateString()))])
-
-result.map((v)=>console.log(v.join(', ')))  
-};
+// Сведённая таблица по всем сотрудникам
+const result = [...employees.entries()].map(([_, { name, work }]) => [
+  name,
+  ...work,
+]);
+result.unshift([
+  "Name/Date",
+  ...A.map((d) => new Date(d).toISOString().slice(0, 10)),
+]);
+result.map((v) => console.log(v.join(", ")));
