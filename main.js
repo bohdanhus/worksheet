acccac38fcce22c488170d2c18320fff00d96f47
bromanovich99 <73001876+bromanovich99@users.noreply.github.com>
2021-04-17 17:08:51 +0300

    adding the main logic

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
+const empty = "0";
 //Получить массив обьектов в переменную data.
-//Чтение сsv файла синхронно с указаным разделителем ","ю
+//Чтение сsv файла синхронно с указаным разделителем ","
 const data = fs
   .readFileSync("acme_worksheet.csv")
   .toString()
@@ -12,23 +13,23 @@ const columns = data.shift();
 const columnName = columns.findIndex((v) => /Name/i.test(v));
 const columnDate = columns.findIndex((v) => /Date/i.test(v));
 const columnWork = columns.findIndex((v) => /Work/i.test(v));
-
-const A = data// Сортировка дат.
+// Сортировка дат.
+const A = data  
   .reduce((a, v) => {
     const date = new Date(v[columnDate]).valueOf();
     a.includes(date) || a.push(date);
     return a;
   }, [])
   .sort();
-
 // Разбор данных для сопоставления
 const employees = data.reduce((a, v) => {
   const name = v[columnName].trim().replace(/\s+/g, " ");
   const date = new Date(v[columnDate]).valueOf();
   const work = v[columnWork].trim();
+  // Сопоставить столбцы c помощью регулярных выражений, заполняем 0 для тех дат, когда сотрудник отсутствует.
   let nw = [...a.entries()].find(([re]) => re.test(name));
-  if (!nw) { // Сопоставить столбцы, заполняем 0 для тех дат, когда сотрудник отсутствует.
-    nw = [null, { name, work: new Array(A.length).fill("0") }];
+  if (!nw) { 
+    nw = [null, { name, work: new Array(A.length).fill(empty) }];
     a.set(new RegExp(name, "i"), nw[1]);
   }
   nw[1].work[A.indexOf(date)] = work;
@@ -45,7 +46,6 @@ result.unshift([
   ...A.map((d) => new Date(d).toISOString().slice(0, 10)),
 ]);
 result.map((v) => console.log(v.join(", ")));
-
 // Записать данные в csv file с помощю функции.
 fs.writeFile("./data.csv", result.join("\r\n"), (err) => {
     console.log(err || "done");
